7c0f86521df0775b81f524e51bf67660453898a7
mengbingzc <13699200852@163.com>
2022-11-24 11:55:07 +0800

    修改upload.js

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,22 +1,22 @@
 const ci = require('miniprogram-ci')
 const { config } = require('./config')
-const fs = require('fs');
-(async () => {
+const fs = require('fs')
+
+;(async () => {
     const project = new ci.Project({
         appid: config.appid,
         type: config.compileType,
         projectPath: config.projectPath,
         privateKeyPath: config.privateKeyPath,
-        ignores: ['node_modules/**/*', `${config.cloudFuncRoot}**/*`,
-            `${config.cloudFuncLib}/**/*`,
-            'scripts/*'],
+        ignores: ['node_modules/**/*', `${config.cloudFuncRoot}**/*`, `${config.cloudFuncLib}/**/*`, 'scripts/*'],
     })
 
-    let descTextBuffer = fs.readFileSync(config.descTextPath);
+    const descTextBuffer = fs.readFileSync(config.descTextPath);
+
     const uploadResult = await ci.upload({
         project,
         version: config.version,
-        desc: descTextBuffer,
+        desc: descTextBuffer.toString(),
         setting: {
             es6: true,
             es7: true,
